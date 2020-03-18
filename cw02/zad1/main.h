57db4cc2a6acc55c8639970876fc008a0330ca85
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-03-18 01:35:17 +0100

    Add main.h and correct .gitignore

---
+++ cw02/zad1/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void error(char *message);
+void start();
+void parseGenerating(char *argv[], int i, int argc);
+void parseSorting(char *argv[], int i, int argc);
+void parseCopying(char *argv[], int i, int argc);
+void generate(char * fileName, int numOfRecords, int recordSize);
+void end(char *nameOfOperation);
+void sysCopy(char * fileFrom, char * fileTo, int numOfRecords, int recordSize);
+void libCopy(char * fileFrom, char * fileTo, int numOfRecords, int recordSize);
+void sysSort(char * fileName, int numOfRecords, int recordSize);
+void sysQuickSort(int file, int numOfRecords, int recordSize, int low, int high);
+int sysPartition(int file, int numOfRecords, int recordSize, int low, int high);
+void sysSwapInFile(int file, int numOfRecords, int recordSize, int i, int j);
+void libSort(char * fileName, int numOfRecords, int recordSize);
+void libQuickSort(FILE * file, int numOfRecords, int recordSize, int low, int high);
+int lilbPartition(FILE * file, int numOfRecords, int recordSize, int low, int high);
+void libSwapInFile(FILE * file, int numOfRecords, int recordSize, int i, int j);
+int main(int argc, char * argv[]);
+
+#endif
