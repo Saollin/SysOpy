90c919b83dcf8cfdde30d7ee174bc863da256c3f
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-03-19 23:00:21 +0100

    Add file nftwFind.c

---
+++ cw02/zad2/nftwFind.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <sys/stat.h> //struct stat
+#include <dirent.h>
+#include <ftw.h>
+#include <limits.h> //PATH_MAX
+
+char *toString(time_t time) {
+    char *string = malloc(30 * sizeof(char));
+    strftime(string, 30, "%Y-%m-%d,%H:%M:%S", localtime(&time));
+    return rstring;
+}
+
