1ec0957104e4547c2b0415555a28f26e08918d85
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-03-19 23:14:02 +0100

    Add function error

--- cw02/zad2/nftwFind.c
+++ cw02/zad2/nftwFind.c
@@ -7,6 +7,10 @@
 #include <ftw.h>
 #include <limits.h> //PATH_MAX
 
+void error(char *message) {
+    perror(message);
+    exit(-1);
+}
 char *toString(time_t time) {
     char *string = malloc(30 * sizeof(char));
     strftime(string, 30, "%Y-%m-%d,%H:%M:%S", localtime(&time));
