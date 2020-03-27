d7023f992522f04c058f0829409784ed78589db8
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-03-28 00:56:29 +0100

    Add main_fork.c

---
+++ cw04/zad2/main_fork.c
@@ -0,0 +1,21 @@
+#define _XOPEN_SOURCE 700
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h> //bool
+
+void sigusrHandler(int signum){
+    printf("\nOdebrano sygnal SIGUSR1\n");
+}
+
+int main(int argc, char ** argv) {
+    if(argc != 2) {
+        printf("Wrong number of arguments!\n");
+        printf("You should add one from these options: ignore, handler, mask, pedning");
+        return -1;
+    }
+    
+}
