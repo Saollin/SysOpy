06203665f7ecfd2bbf7308f543a7e5425f5c8734
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-03-28 01:25:50 +0100

    Add parsing arguments in main

--- cw04/zad2/main_fork.c
+++ cw04/zad2/main_fork.c
@@ -5,7 +5,7 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdbool.h> //bool
+#include <string.h>
 
 void error(char *message) {
     perror(message);
@@ -26,10 +26,62 @@ void maskSignal() {
 }
 
 int main(int argc, char ** argv) {
-    if(argc != 2) {
+    if(argc < 2) {
         printf("Wrong number of arguments!\n");
         printf("You should add one from these options: ignore, handler, mask, pedning");
         return -1;
+    }
+    if(!strcmp(argv[1], "ignore")){
+        signal(SIGUSR1, SIG_IGN);
+    }
+    else if(!strcmp(argv[1], "handler")) {
+        signal(SIGUSR1, sigusrHandler);
+    }
+    else if(!strcmp(argv[1], "mask")) {
+        maskSignal();
+    }
+    else if(!strcmp(argv[1], "pending")) {
+        maskSignal;
+    }
+    else {
+        printf("Such option doesn't exist!");
+        printf("You should choose one from these options: ignore, handler, mask, pedning");
+        return -1;
+    }
+
+    raise(SIGUSR1);
+    pid_t child;
+
+    if(!strcmp(argv[1], "ignore")){
+        child = fork();
+        if(child == 0) {
+            raise(SIGUSR1);
+        }
+    }
+    else if(!strcmp(argv[1], "handler")) {
+        child = fork();
+        if(child == 0) {
+            raise(SIGUSR1);
+        }
+    }
+    else if(!strcmp(argv[1], "mask")) {
+        child = fork();
+        if(child == 0) {
+            raise(SIGUSR1);
+        }
+    }
+    else if(!strcmp(argv[1], "pending")) {
+        child = fork();
+        if(child == 0) {
+            sigset_t set;
+            sigpending(&set);
+            if(sigismember(&set, SIGUSR1)) {
+                printf("SIGUSR1 is visible.\n");
+            }
+            else {
+                printf("SIGUSR1 is not visible.\n");
+            }
+        }
     }
     
 }
