b205269fd5f5c3eac066502a05070e3d29a28cd7
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-03-30 00:04:23 +0200

    Add main in catcher.c

--- cw04/zad4a/catcher.c
+++ cw04/zad4a/catcher.c
@@ -22,3 +22,58 @@ void sig2Handler(int sig, siginfo_t * info, void * ucontext) {
     senderPid = info->si_pid;
     isSig2Catched = true;
 }
+
+int main(int argc, char ** argv) {
+    if(argc < 2) {
+        printf("Wrong number of arguments!\n");
+        printf("You should add one from these options: kill, sigrt, sigqueue\n");
+        return -1;
+    }
+    pid_t pid = getpid();
+    printf("Catcher PID number: %d\n", pid);
+
+    int SIG1, SIG2;
+    if(!strcmp(argv[1], "sigrt")){
+        SIG1 = SIGRTMIN + 2;
+        SIG2 = SIGRTMIN + 3;
+    }
+    else {
+        SIG1 = SIGUSR1;
+        SIG2 = SIGUSR2;
+    }   
+
+    struct sigaction act1;
+    sigemptyset(&act1.sa_mask);
+    // sigdelset(&act1.sa_mask, SIG1);
+    // sigdelset(&act1.sa_mask, SIG2);
+    act1.sa_flags = 0;
+    act1.sa_handler = sig1Handler;
+
+    // signal(SIG1, sig1Handler);
+    struct sigaction act2;
+    sigemptyset(&act2.sa_mask);
+    act2.sa_flags = SA_SIGINFO;
+    act2.sa_sigaction = sig2Handler;
+    
+    sigaction(SIG1, &act1, NULL);
+    sigaction(SIG2, &act2, NULL);
+
+    while(!isSig2Catched);
+
+    if(!strcmp(argv[1], "kill") || !strcmp(argv[1], "sigrt")) {
+        for(int i = 0; i < receivedSignals; i++) {
+            kill(senderPid, SIG1);
+        }
+        kill(senderPid, SIG2);
+    }
+    else if(!strcmp(argv[1], "sigqueue")) {
+        union sigval value;
+        value.sival_ptr = NULL;
+        for(int i = 0; i < receivedSignals; i++) {
+            value.sival_int = i;
+            sigqueue(senderPid, SIG1, value);
+        }
+        sigqueue(senderPid, SIG2, value);
+    }
+    return 0;
+}
