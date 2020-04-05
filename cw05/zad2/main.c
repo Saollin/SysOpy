2904089054f7d1d58a04efd4993984f81fb9bc10
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-04-05 19:43:39 +0200

    Add cw05/zad2

---
+++ cw05/zad2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        fprintf(stderr, "Wrong number of arguments. You should give only name of file\n");
+        return 1;
+    }
+    char * fileName = argv[1];
+
+    char buff[200];
+    sprintf(buff, "sort %s", fileName);
+    FILE * sortFile = popen(buff, "r");
+    
+    while(!feof(sortFile)) {
+        fgets(buff, sizeof(buff), sortFile);
+        printf("%s", buff);
+    }
+
+    pclose(sortFile);
+
+    return 0;
+}
